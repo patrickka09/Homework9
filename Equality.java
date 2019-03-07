8e729f8537277f1a5c2d9d955babe3783f1466d3
patrick <kmpatrick@gmail.com>
2019-03-06 20:50:20 -0600

    homework9

---
+++ Equality.java
@@ -0,0 +1,37 @@
+package edu.dmacc.codedsm.hw9;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class Equality {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = generateIntegers(20);
+        for (int i : integers) {
+            System.out.println("i = " + i);
+            if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else if(i %3 == 0 && i % 5 == 0){
+                System.out.println("Fizzbuzz");
+            }
+            else{
+                System.out.println(i+ " is not fizzy");
+            }
+        }
+    }
+
+    public static ArrayList<Integer> generateIntegers(int size) {
+        ArrayList<Integer> xs = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            xs.add(Math.abs(random.nextInt(Integer.MAX_VALUE - 1) + 1));
+        }
+        return xs;
+    }
+}
+
